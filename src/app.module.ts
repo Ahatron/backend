634eaa3b79b1b04057e3112d96e6a1461473d3f1
Ahatron <ahatpolatov@gmail.com>
2024-09-18 21:37:37 +0500

    mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+// import { AuthModule } from './auth/auth.module';
+// import { NotesModule } from './notes/notes.module';
+import { AuthModule } from './auth/auth.module';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-notes-app'),
+    AuthModule,
+    NotesModule, // Ваша строка подключения
+    // AuthModule,
+    // NotesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
